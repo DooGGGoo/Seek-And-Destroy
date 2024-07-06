22e03757d685604632262152a8bb6dc8d20f32e3
dgg <dooggoowtfok@gmail.com>
2024-07-06 03:33:53 +0300

    Radar things

--- src/main/java/mod/seekndestroy/block/entity/GroundRadarBlockEntity.java
+++ src/main/java/mod/seekndestroy/block/entity/GroundRadarBlockEntity.java
@@ -7,8 +7,6 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.particle.ParticleEffect;
-import net.minecraft.particle.ParticleType;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.screen.PropertyDelegate;
@@ -28,9 +26,10 @@ public class GroundRadarBlockEntity extends BlockEntity
 	private int progress = 0;
 	private final int maxProgress = 360;
 
-	private final int scanRate = 3;
-	private final int scanRadius = 50;
-	private final float scanAngle = 35f;
+	private final int scanRate = 24;
+	private final int scanRadius = 200;
+	private final float scanAngle = 60f;
+	//private float scanElevationAngle = 25f;
 
 	public GroundRadarBlockEntity(BlockPos pos, BlockState state)
 	{
@@ -70,48 +69,55 @@ public class GroundRadarBlockEntity extends BlockEntity
 	{
 		if (blockEntity instanceof GroundRadarBlockEntity groundRadarBlockEntity)
 		{
-			groundRadarBlockEntity.progress += groundRadarBlockEntity.scanRate;
-
-			if (groundRadarBlockEntity.progress >= groundRadarBlockEntity.maxProgress)
+			if (world.getTime() % 2 == 0)
 			{
-				world.playSound(null, pos, SoundEvents.BLOCK_BELL_USE, SoundCategory.BLOCKS, 0.6f, 1.2f);
-				groundRadarBlockEntity.progress = 0;
+				tickRadar(world, pos, groundRadarBlockEntity);
 			}
 
-			Vec3d radarDirection = MathUtils.intToDirectionVector(groundRadarBlockEntity.progress);
-
-			// spawn particle at direction
-
-			world.addImportantParticle(ParticleTypes.FLAME, pos.getX() + 0.5f + radarDirection.getX() * 2f, pos.getY() + 0.5f, pos.getZ() + 0.5f + radarDirection.getZ() * 2f, 0.0, 0.0, 0.0);
-
-			final Box scanBox = new Box(pos).offset(0, -10, 0)
-					.expand(groundRadarBlockEntity.scanRadius, 0, groundRadarBlockEntity.scanRadius)
-					.stretch(0, 50, 0);
-
-			List<Entity> entities = world.getOtherEntities(null, scanBox, entity -> entity instanceof LivingEntity);
-			for (Entity entity : entities)
-			{
-				if (entity != null)
-				{
-					Vec3d entityPos = entity.getPos();
-					final Vec3d originPos = MathUtils.toVec3d(pos);
-					Vec3d dir = new Vec3d(radarDirection.getX(), pos.getY() + 0.5f, radarDirection.getZ());
-
-					if (MathUtils.isPointInsideCone(originPos, entityPos, radarDirection, groundRadarBlockEntity.scanRadius * 1.33f, groundRadarBlockEntity.scanAngle))
-					{
-						if (world.isClient())
-						{
-							world.playSoundAtBlockCenter(entity.getBlockPos(), SoundEvents.BLOCK_COPPER_BULB_TURN_ON, SoundCategory.BLOCKS, 0.8f, 1.1f, true);
-						}
-						world.addParticle(ParticleTypes.NOTE, entityPos.getX(), entityPos.getY(), entityPos.getZ(), 0.0, 0.0, 0.0);
-					}
-
-					// System.out.println(dir + " | " + groundRadarBlockEntity.progress + " | " + MathUtils.isPointInsideCone(originPos, entityPos, radarDirection, groundRadarBlockEntity.scanRadius * 1.33f, groundRadarBlockEntity.scanAngle));
-				}
-			}
 			if (world.getTime() % 20 == 0)
 			{
 				groundRadarBlockEntity.markDirty();
+			}
+		}
+	}
+
+	private static void tickRadar(World world, BlockPos pos, GroundRadarBlockEntity groundRadarBlockEntity)
+	{
+		groundRadarBlockEntity.progress += groundRadarBlockEntity.scanRate;
+
+		if (groundRadarBlockEntity.progress >= groundRadarBlockEntity.maxProgress)
+		{
+			world.playSound(null, pos, SoundEvents.BLOCK_BELL_USE, SoundCategory.BLOCKS, 0.6f, 1.2f);
+			groundRadarBlockEntity.progress = 0;
+		}
+
+		Vec3d radarDirection = MathUtils.intToDirectionVector(groundRadarBlockEntity.progress);
+
+		// spawn particle at direction
+		world.addImportantParticle(ParticleTypes.FLAME, pos.getX() + 0.5f + radarDirection.getX() * 2f, pos.getY() + 1f, pos.getZ() + 0.5f + radarDirection.getZ() * 2f, 0.0, 0.0, 0.0);
+
+		final Box scanBox = new Box(pos).offset(0, -10, 0)
+				.expand(groundRadarBlockEntity.scanRadius, 0, groundRadarBlockEntity.scanRadius)
+				.stretch(0, 50, 0);
+
+		List<Entity> entities = world.getOtherEntities(null, scanBox, entity -> entity instanceof LivingEntity);
+		for (Entity entity : entities)
+		{
+			if (entity != null)
+			{
+				Vec3d entityPos = entity.getPos();
+				final Vec3d originPos = MathUtils.toVec3d(pos);
+				Vec3d dir = new Vec3d(radarDirection.getX(), pos.getY() + 1f, radarDirection.getZ());
+
+				if (MathUtils.isPointInsideCone(originPos, entityPos, dir, groundRadarBlockEntity.scanRadius * 1.33f, groundRadarBlockEntity.scanAngle))
+				{
+					if (world.isClient())
+					{
+						world.playSoundAtBlockCenter(entity.getBlockPos(), SoundEvents.BLOCK_COPPER_BULB_TURN_ON, SoundCategory.BLOCKS, 0.8f, 1.1f, true);
+					}
+					world.addParticle(ParticleTypes.NOTE, entityPos.getX(), entityPos.getY(), entityPos.getZ(), 0.0, 0.0, 0.0);
+				}
+				// System.out.println(dir + " | " + groundRadarBlockEntity.progress + " | " + MathUtils.isPointInsideCone(originPos, entityPos, radarDirection, groundRadarBlockEntity.scanRadius * 1.33f, groundRadarBlockEntity.scanAngle));
 			}
 		}
 	}
